4967bbf9f731c24f223021d7b2b869e52ae40010
Suraj Soma <ssoma@videologygroup.com>
2016-06-23 17:28:17 +0530

    Modified myFunction

--- app/src/main/java/com/cybage/surajso/helloworld/MainActivity.java
+++ app/src/main/java/com/cybage/surajso/helloworld/MainActivity.java
@@ -4,9 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    protected void myFunction(){
-        //Sample function
-    }
+    protected void myFunction(){ /*sample function*/ }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
